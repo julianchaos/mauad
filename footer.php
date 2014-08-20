44dfc3070cd74d22913a55301ebf1362b2da890f
Julian Andrade <julianandrade@agenciaumbrella.com.br>
2014-08-20 20:56:38 -0300

    Definição do footer, nos 4 tamanhos do Bootstrap.

--- footer.php
+++ footer.php
@@ -8,15 +8,13 @@
 			<footer>
 				<div class="container">
 					<div class="row" role="contentinfo">
-						<div>
+						<div class="col-xs-12 col-sm-10 mauad-footer-info">
 							<a href="<?php echo home_url() ?>/" title="<?php bloginfo('name') ?>">
 								<img src="<?php bloginfo('template_directory') ?>/images/logo-footer.png" class="img-responsive" alt="<?php bloginfo('name') ?>" />
 							</a>
-						</div>
-						<div>
 							<p>Copyright &copy; Mauad 2014. All Rights Reserved</p>
 						</div>
-						<div class="col-xs-4 col-sm-3 col-md-2">
+						<div class="col-xs-12 col-sm-2 assinaturas">
 							<a href="http://www.rds.com.br/" target="_BLANK">
 								<img src="<?php bloginfo('template_directory') ?>/images/logo-rds-footer.png" class="img-responsive" alt="RDS" />
 							</a>
