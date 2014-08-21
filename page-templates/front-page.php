06f75cec0aee3e7f882acbea59c9732b51c8aa16
Julian Andrade <julianandrade@agenciaumbrella.com.br>
2014-08-20 21:34:42 -0300

    Conteúdo da página principal no tamanho Small Devices

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -20,19 +20,23 @@
 		<ol class="carousel-indicators">
 			<li data-target="#carousel-generic" data-slide-to="0" class="active"></li>
 			<li data-target="#carousel-generic" data-slide-to="1"></li>
-			<li data-target="#carousel-generic" data-slide-to="2"></li>
 		</ol>
 
 		<!-- Wrapper for slides -->
 		<div class="carousel-inner">
 			<div class="item active">
-				<img src="<?php bloginfo('template_directory') ?>/images/slider/Slider-01.jpg" alt="..." />
+				<img src="<?php bloginfo('template_directory') ?>/images/slide/slide-01.jpg" alt="..." />
+				<div class="carousel-caption">
+					<h2>Fundação Oscar Niemayer</h2>
+					<h3>Rio de Janeiro</h3>
+				</div>
 			</div>
 			<div class="item">
-				<img src="<?php bloginfo('template_directory') ?>/images/slider/Slider-02.jpg" alt="..." />
-			</div>
-			<div class="item">
-				<img src="<?php bloginfo('template_directory') ?>/images/slider/Slider-03.jpg" alt="..." />
+				<img src="<?php bloginfo('template_directory') ?>/images/slide/slide-01.jpg" alt="..." />
+				<div class="carousel-caption">
+					<h2>Teste</h2>
+					<h3>teste</h3>
+				</div>
 			</div>
 		</div>
 
@@ -44,16 +48,14 @@
 			<span class="glyphicon glyphicon-chevron-right"></span>
 		</a>
 	</div>
-	<h2 class="text-center h3">A imaginação pode nos levar a lugares incríveis...</h2>
 	<div id="primary">
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-12 col-sm-8 col-md-8 col-lg-7">
-					<h3>... e, muitas vezes, estes lugares estão pertinho de nós.</h3>
-					<p>Desde criança nós sonhamos com lugares incríveis. As brincadeiras queestimulam a imaginação, nos levam para o espaço sideral, cavernas, ilhas do tesouro, para o fundo dos oceanos, florestas encantadas e diversos outros cenários imaginários que ilustram nossas fantasias.</p>
-					<p>Quando nos tornamos adultos, continuamos sonhando com outros lugares incríveis que vimos em algum filme ou revista, desejamos conhecer outrospaíses, outras culturas e nações ao redor do planeta. Desta vez, os sonhos já não são mais fantasias e se tornam totalmente possíveis.</p>
-					<p>Em Petrópolis, existe um lugar especial, daqueles que todo mundo sonha. Planejado para viver com conforto, segurança, qualidade de vida, lazer e o melhor: a poucos minutos, das melhores atrações que você só encontra na Cidade Imperial e em Itaipava.</p>
-					<p>Viva o mellhor dos dois mundos. Petrópolis e Itaipava a poucos minutos da sua casa. Desfrute do melhor que Petrópolis pode oferecer em produtos e serviços, diversão e lazer e curta também todo a sofisticação, gastronomia,decoração, moda e todas as diversas atrações de Itaipava.<br>Conheça agora o empreendimento que vai colocar você no ponto mais estratégico de toda a Região Serrana.</p>
+				<div class="col-xs-12">
+					<h1>Mauad</h1>
+					<p>Da segunda geração de uma família de arquitetos nasceu o desejo de resgatar o trabalho de arquitetura pela assunção da responsabilidade e coordenação de todas as fases do empreendimento.</p>
+					<p>Entendemos que a harmonia e a beleza de uma construção não se encerram no projeto, nascem dele e se consolidam na obra acabada, depois de retirado o último andaime.</p>
+					<p>Desejamos criar e, mais do que isso, essencialmente, realizar.</p>
 				</div>
 			</div>
 		</div>
